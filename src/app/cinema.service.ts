fc8355e0876a2e12d0786ffeedbd340c6f5bdc18
Lucas Oliveira Costa <lucasocosta@hotmail.com>
2019-11-12 11:23:52 -0300

    editar/remover

--- src/app/cinema.service.ts
+++ src/app/cinema.service.ts
@@ -18,4 +18,19 @@ export class CinemaService {
   {
     return this.http.post("http://localhost:3000/cinema", cinema);
   }
+
+  getCinema(idcinemas: number): Observable<Cinema>
+  {
+    return this.http.get<Cinema>("http://localhost:3000/cinema/" + idcinemas);
+  }
+
+  remover(idcinemas: number): Observable<any>
+  {
+    return this.http.delete("http://localhost:3000/cinema/" + idcinemas);
+  }
+
+  edit(cinema: Cinema): Observable<any>
+  {
+    return this.http.put("http://localhost:3000/cinema/" + cinema.idcinemas,cinema);
+  }
 }
--- src/app/cinema/cinema.component.ts
+++ src/app/cinema/cinema.component.ts
@@ -21,31 +21,54 @@ export class CinemaComponent implements OnInit {
   ngOnInit() {
     this.novoModal = new Cinema();
     this.service.getCinemas().subscribe(cinemas => this.cinemas = cinemas);
-    this.opcaoModal=1;
+    this.opcaoModal=-1;
   }
 
   adicionarNovo()
   {
-    this.service.adicionar(this.novoModal).subscribe(res => {
-      this.novoModal.idcinemas = res.insertId;
-      this.cinemas.push(this.novoModal);
-    });    
-    this.basic = false;
+    this.novoModal= new Cinema();
+    this.basic = true;
+    this.opcaoModal=1;
   }
 
-  editar()
+  
+  editar(cinema: Cinema)
   {
+    this.opcaoModal=0;
+    this.service.getCinema(cinema.idcinemas).subscribe(res =>
+    {
+      this.novoModal = cinema;
+      this.basic = true;
+    });
 
+  }
+
+  remover(cinema: Cinema)
+  {
+    this.service.remover(cinema.idcinemas).subscribe(res =>
+    {
+      let cinemaIdx = this.cinemas.indexOf(cinema);
+      this.cinemas.splice(cinemaIdx,1);
+    });
   }
 
   salvar() {
     if(this.opcaoModal==1)
     {
-      this.adicionarNovo()
+      this.service.adicionar(this.novoModal).subscribe(res => {
+        this.novoModal.idcinemas = res.insertId;
+        this.cinemas.push(this.novoModal);
+      });    
+      this.basic = false;
+  
     }
     else if(this.opcaoModal==0)
     {
-      this.editar()
+       this.service.edit(this.novoModal).subscribe(res => {
+        let cinemaIdx = this.cinemas.findIndex(c => c.idcinemas == this.novoModal.idcinemas)
+        this.cinemas[cinemaIdx] = this.novoModal;
+       });
+       this.basic = false;
     }
     
   }
@@ -53,6 +76,7 @@ export class CinemaComponent implements OnInit {
   cancelar() {
     this.novoModal = new Cinema();
     this.basic = false;
+    
   }
 }
 
